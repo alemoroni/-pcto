4ce3fb643dd3165ba70fdbb6f96816b34ada4912
alemoroni <162116948+alemoroni@users.noreply.github.com>
2024-03-04 16:00:28 +0000

    f

---
+++ esercizio00.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+printf("magic mike\n");
+return(0);
+
+}
+
---
+++ esercizio01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+int a=9;
+int b=13;
+int c=a*b;
+printf("ciao %d * %d = %d",a, b, c);
+}
